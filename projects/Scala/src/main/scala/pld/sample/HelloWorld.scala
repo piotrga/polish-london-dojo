a2ef93dc2b9512df1079b315121827f5fa052c44
Piotr Gabryanczyk <piotrga@crystalbrains.com>
2010-04-28 16:06:15 +0100

    SWING sample

--- projects/Scala/src/main/scala/pld/sample/HelloWorld.scala
+++ projects/Scala/src/main/scala/pld/sample/HelloWorld.scala
@@ -1,7 +1,23 @@
 package pld.sample
 
+import java.awt.{LayoutManager, Color, Graphics}
+import javax.swing.{BoxLayout, JPanel, JFrame}
+
 object HelloWorld {
   def main(args: Array[String]) {
     println("Hello, world!")
+
+    var frame : JFrame = new JFrame()
+    frame.setSize(250, 250)
+    
+    val panel: JPanel = new JPanel() {
+      override def paint(g: Graphics) = {
+        g.setColor(Color.BLUE)
+        g.drawRoundRect(10, 10, 180, 180, 50, 50)
+      }
+    }
+    frame.getContentPane().add(panel)
+    frame.setVisible(true)
+    
   }
 }
